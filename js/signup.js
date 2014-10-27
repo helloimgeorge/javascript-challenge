2f38b9818384a889fc7306d43e641f09ce918a75
George Yu <yugeorge@uw.edu>
2014-10-26 22:09:01 -0700

    v2

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,31 @@
-/*
-    Signup Form Script
-    This script will load the state select list and validate the form before submission
-*/
+"strict use";
+
+
+
+function onReady() {
+    var stateForm = document.getElementById('signup');
+    var stateSelect = stateForm.elements['state'];
+
+    var idx;
+    var option;
+
+    for (idx = 0; idx < usStates.length; ++idx) {
+        option = document.createElement('option');
+        option.innerHTML = usStates[idx].name;
+        option.value = usStates[idx].code;
+        stateSelect.appendChild(option);
+    }
+}
+
+document.addEventListener('change', function() {
+    var option = document.getElementById('occupation');
+    console.log(option.value);
+
+    if (option.value == 'other') {
+        document.getElementById("occupationOther").style.display = "block";
+    } else {
+        document.getElementById("occupationOther").style.display = "none";
+    }
+});
+
+document.addEventListener('DOMContentLoaded', onReady);
