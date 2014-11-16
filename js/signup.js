4c8e6f14812545fd92601fd4333d01c8a1e77aeb
George <yugeorge@uw.edu>
2014-11-16 04:57:57 -0800

    v5

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,7 @@
-"strict use";
+"use strict";
 
-document.addEventListener('DOMContentLoaded', onReady);
+document.addEventListener('DOMContentLoaded', function() {
 
-function onReady() {
     var stateForm = document.getElementById('signup');
     var stateSelect = stateForm.elements['state'];
 
@@ -16,7 +15,20 @@ function onReady() {
         stateSelect.appendChild(option);
     }
 
-    document.addEventListener('change', function() {
+    // cancel button
+    var buttonConfirmation = document.getElementById('cancelButton');
+
+    buttonConfirmation.addEventListener('click', function () {
+        if (window.confirm('Are you really sure you want to leave?')) {
+            window.location = 'http://www.google.com';
+        }
+    });
+
+    // submit button
+    signup.addEventListener('submit', onSubmit);
+
+    // if occupation other
+    document.addEventListener('change', function () {
         var option = document.getElementById('occupation');
 
         if (option.value == 'other') {
@@ -26,82 +38,78 @@ function onReady() {
         }
     });
 
-    var buttonConfirmation = document.getElementById('cancelButton');
-
-    buttonConfirmation.addEventListener('click', function() {
-        if (window.confirm('Are you really sure you want to leave?')) {
-            window.location = 'http://www.google.com';
+    // on submit
+    function onSubmit(evt) {
+        evt.returnValue = validateForm(this);
+        evt.returnvalue = ageCheck(this);
+        if (!evt.returnValue && evt.preventDefault) {
+            evt.preventDefault();
         }
-    });
+        return evt.returnValue;
+    } // end onSubmit method
 
-    signup.addEventListener('submit', onSubmit);
-} // onReady
+    // validate form by certain required fields
+    function validateForm(form) {
+        var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
 
+        var option = document.getElementById('occupation');
 
-function onSubmit(evt) {
-    var dob = this.elements['birthdate'].value;
+        if (option.value == 'other') {
+            requiredFields.push('occupationOther');
+        }
 
-    evt.returnValue = validateForm(this);
-    var age = calculateAge(dob);
-    console.log(age);
-    if (!evt.returnValue && evt.preventDefault && age < 14) {
+        var idx;
+        var formValid = true;
+        for (idx = 0; idx < requiredFields.length; ++idx) {
+            formValid &= validateRequiredField(form.elements[requiredFields[idx]]);
+        }
+        return formValid;
+    } // end validateForm method
+
+    // validate specific field
+    function validateRequiredField(field) {
+        var value = field.value.trim(); //need to trim entered values so its not counted
+        var valid = value.length > 0;
+
+        if (field.id == 'zip') {
+            valid = zipCheck(field);
+        }
+        if (valid) {
+            field.className = 'form-control';
+        }
+        else {
+            field.className = 'form-control invalid invalid-form';
+        }
+        return valid;
+    } // end validateRequiredFields method
+
+    // calculate the age of user
+    function calculateAge(dob) {
+        if (!dob) {
+            throw new Error('Please tell me when you were born!');
+        }
+        return moment().diff(dob, 'years');
+    } // end calculateAge method
+
+    // check if zipcode is 5 digits
+    function zipCheck(field) {
+        var res = new RegExp('^\\d{5}$').test(field.value.trim());
+        return res;
+    } // end zhipcheck method
+
+    // check whether age is greater than 13
+    function ageCheck(evt) {
+        var dob = evt.elements['birthdate'].value;
+        var age = calculateAge(dob);
+        var msgElem = document.getElementById('birthdateMessage');
         if (age < 14) {
             birthdate.className = 'form-control invalid invalid-form';
-            var msgElem = document.getElementById('birthdateMessage');
+            msgElem.style.display = "block";
             msgElem.innerHTML = 'You must be 13 years or older to sign up';
-        }
-        evt.preventDefault();
-    }
-
-    return evt.returnValue;
-
-} //onSubmit()
-
-function validateForm(form) {
-    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
-
-    var option = document.getElementById('occupation');
-
-    if (option.value == 'other') {
-        requiredFields.push('occupationOther');
-    }
-
-    var idx;
-    var formValid = true;
-    for (idx = 0; idx < requiredFields.length; ++idx) {
-        formValid &= validateRequiredField(form.elements[requiredFields[idx]]);
-    }
-
-    return formValid;
-
-} //validateForm()
-
-function validateRequiredField(field) {
-    var value = field.value.trim(); //need to trim entered values so its not counted
-    var valid = value.length > 0;
-
-    if (field.id == 'zip') {
-        var zipRegExp = new RegExp('^\\d{5}$');
-        var res = zipRegExp.test(value);
-        if (res) {
-            field.className = 'form-control';
         } else {
-            valid = false;
-            field.className ='form-control invalid invalid-form';
+            birthdate.className = 'form-control';
+            msgElem.style.display = "none";
         }
-    } else if (valid) {
-        field.className = 'form-control';
-    }
-    else {
-        field.className = 'form-control invalid invalid-form';
-    }
-
-    return valid;
-} // end validateRequiredFields
-
-function calculateAge(dob) {
-    if (!dob) {
-        throw new Error('Please tell me when you were born!');
-    }
-    return moment().diff(dob, 'years');
-} // calculateAge method
+        return age < 14;
+    } // end ageCheck method
+});
